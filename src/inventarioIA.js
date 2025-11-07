17135b096b0a8ee03ecb7af952e38de83c96766a
jmcastilla <jmcastilla91@gmail.com>
2025-11-07 09:47:59 -0500

    Prueba

--- src/inventarioIA.js
+++ src/inventarioIA.js
@@ -64,13 +64,12 @@ async function saveToDatabase(items) {
     database: "agenteia"
   });
 
-  console.log(`[${new Date().toISOString()}] Limpiando Base de datos...`);
-  await conn.execute('DELETE FROM inventarios');
-
   const batchSize = 1000;
   const insertQuery = `
-    INSERT INTO inventarios (sku, bod, stock)
+    INSERT INTO inventarios AS new (sku, bod, stock)
     VALUES ?
+    ON DUPLICATE KEY UPDATE
+      stock = new.stock
   `;
 
   for (let i = 0; i < items.length; i += batchSize) {
